ebff5684b11eca52e19abf06d0dc76bf9a42ee1d
Fonkarts <sebastienhouchet@gmail.com>
2023-12-24 13:34:22 +0100

    update structure & conf

--- xlsx_analyzer_front/src/main.tsx
+++ xlsx_analyzer_front/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./App.scss";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+    const root = createRoot(rootElement);
+
+    root.render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </React.StrictMode>
+    );
+} else {
+    console.log("error");
+}
