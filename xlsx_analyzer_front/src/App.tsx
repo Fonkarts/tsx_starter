b7e34a642608c00613efdf07dd6b493d999cd3cd
Fonkarts <sebastienhouchet@gmail.com>
2023-12-25 20:44:27 +0100

    add Header

--- xlsx_analyzer_front/src/App.tsx
+++ xlsx_analyzer_front/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Routes from "./router/Routes";
 import "./app.scss";
+import Header from "./components/Header/Header";
 
 const App: React.FC = () => {
     return (
         <div className="app">
-            <h1>ExcelAnt, your xls analyzer !</h1>
+            <Header />
             <Routes />
         </div>
     );
---
+++ xlsx_analyzer_front/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <img src="/small_xlant.png" alt="xlant app logo" />
+            <h1>XLAnt, your xls files analyzer !</h1>
+        </header>
+    );
+};
+
+export default Header;
