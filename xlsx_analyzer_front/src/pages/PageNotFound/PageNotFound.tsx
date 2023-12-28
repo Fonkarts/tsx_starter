04986afc638ed869a60bbd6d5b49be3340d6515b
Fonkarts <sebastienhouchet@gmail.com>
2023-12-28 17:25:48 +0100

    add Card wrapper into page not found

--- xlsx_analyzer_front/src/pages/PageNotFound/PageNotFound.tsx
+++ xlsx_analyzer_front/src/pages/PageNotFound/PageNotFound.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import Card from "../../components/Card/Card";
 
 const PageNotFound: React.FC = () => {
     return (
         <section className="pageNotFound">
-            <h2>Page not found !</h2>
-            <p>It seems that the page you are looking for doesn't exist.</p>
-            <p>
-                Get Back to the <Link to={"/"}>Homepage</Link>
-            </p>
+            <Card>
+                <h2>Page not found !</h2>
+                <p>It seems that the page you are looking for doesn't exist.</p>
+                <p>
+                    Get Back to the <Link to={"/"}>Homepage</Link>
+                </p>
+            </Card>
         </section>
     );
 };
